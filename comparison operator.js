4e709072545efbcbe4ca290772082137f9dab2ed
Atharv Ravikumar Ghadage <atharvghadage464@gmail.com>
2025-06-23 11:36:10 +0530

    Create comparison operator.js

---
+++ comparison operator.js
@@ -0,0 +1,45 @@
+
+//greater than
+var a=50;
+var b=7;
+console.log(a>b);//true
+console.log(a<b);//false
+
+//less than
+var a=21;
+var b=45;
+console.log(a<b);//true
+console.log(a>b);//false
+
+//greater tham=nequal to
+var a=30;
+var b=30;
+var c=a>=b;
+console.log(c);//true
+
+var e=55;
+var f=56;
+var g=e>=f;
+console.log(g);//false
+
+//less than equal to
+var b=10;
+var a=10;
+var c=b<=a;
+console.log(c);//true
+
+var h=20;
+var g=19;
+var i=h<=g;
+console.log(i);//false
+//== operator and === operator
+var a=14;
+var b="14";
+console.log(a==b);//true
+console.log(a===b);//false
+
+// != operator and !== operator
+var d=22;
+var e="22";
+console.log(d!=e);//false
+console.log(d!==e);//true
