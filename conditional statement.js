dd0cd1ec18cfdf8dfbb26595effead861ab5a3f1
Atharv Ravikumar Ghadage <atharvghadage464@gmail.com>
2025-06-27 11:23:58 +0530

    Create conditional statement.js
    if-else condition and also if -else and else if condition

---
+++ conditional statement.js
@@ -0,0 +1,69 @@
+var user="atharv";
+if(user=="atharv"){
+  console.log("welcome atharv");
+}else{
+  console.log("user not found");
+}
+
+var age=18;
+if (age>=18){
+  var a=2;
+  var b=3;
+  console.log(a+b);
+  console.log("you can vote");
+} else{
+  console.log("you cannot vote");
+}
+
+var age=16;
+if (age>=18){
+  var a=2;
+  var b=3;
+  console.log(a+b);
+  console.log("you can vote");
+} else{
+  console.log("you cannot vote");
+}
+
+////////////////////////////////////////////////////////////else if condition ///////////////////////////////////////////////////////////////
+var temp=40;
+if(temp==40){
+  console.log("normal day");
+}else if(temp>40){
+  console.log("hot day");
+}else if(temp<40){
+  console.log("cold day");
+}else{
+  console.log("invalid data");
+}
+
+var score=90;
+if(score>=90){
+  console.log("grade o");
+}else if(score>=85){
+  console.log("grade a+");
+}else if(score>=80){
+  console.log("grade a");
+}else if(score>=70){
+  console.log("grade b");
+}else{
+  console.log("student is fail");
+}
+
+////////////////////////////////////////////////not operator(!)///////////////////////////////////////////////////////
+console.log(!true);
+console.log(!false);
+
+var lightoff=false;
+if(!lightoff){
+  console.log("turn on the light");
+}else{
+  console.log("turn off the light");
+}
+
+var lightoff=true;
+if(!lightoff){
+  console.log("turn on the light");
+}else{
+  console.log("turn off the light");
+}
