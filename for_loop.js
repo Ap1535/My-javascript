6e7b0cadaa7642a8a5948f6f21faeba9dd54b9b6
Atharv Ravikumar Ghadage <atharvghadage464@gmail.com>
2025-06-28 22:09:24 +0530

    Create for_loop.js

---
+++ for_loop.js
@@ -0,0 +1,45 @@
+for(var i=1;i<5;i++){
+  console.log("hello");
+}
+
+for(var i=0;i<=7;i++){
+  console.log(i);
+}
+//i want output  in single line
+var bag=" ";
+for(var i=0;i<=8;i++){
+  bag=bag+i+" ";
+  
+}
+console.log(bag);
+
+
+
+
+//addition of 1 to 10
+sum=0;
+for(i=1;i<=10;i++){
+    sum=sum+i;
+
+}
+console.log(sum);
+
+///////////////
+
+var bag=" ";
+for(var i=0;i<=8;i++){
+  bag=bag+i+" ";
+  console.log(bag);
+}
+
+
+//reverce for loop
+for(var i=10;i>=1;i--){
+  console.log(i);
+}
+//////////////////////////
+var bag=" ";
+for(var i=8;i>=0;i--){
+  bag=bag+i+" ";
+  console.log(bag);
+}
